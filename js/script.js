1caa700a3e8800bfde0d327a3008f1d1e76d8ff9
unknown <you@example.com>
2023-09-07 15:29:22 +0600

    BWCCI

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$('.gallery_parts').slick({
+    centerMode: true,
+    infinite: true,
+    arrows:false,
+    autoplay: true,
+    dots: true,
+    centerPadding: '0px',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '60px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
